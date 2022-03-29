5b0b54afc935673ec947ddda797941356ae9f4c8
Chris Nesler <neslerc@umich.edu>
2022-03-29 17:53:55 -0400

    adding slider bar stuff

--- opencvWork_chris.py
+++ opencvWork_chris.py
@@ -4,6 +4,25 @@ import sys
 import cv2
 import numpy as np
 
+hue_val_low = 32768
+hue_val_high = 32768
+
+
+def on_trackbar_hueLO(val):
+    global hue_val_low
+    hue_val_low = val
+    show_image()
+
+def on_trackbar_hueHI(val):
+    global hue_val_high
+    hue_val_high = val
+    show_image()
+
+def show_image():
+    RedMask = cv2.inRange(imgMasked_hsv,hue_val_low, hue_val_high)
+    redsOnly = cv2.bitwise_and(imgMasked_rgb,imgMasked_rgb,mask=RedMask)
+    cv2.imshow("Threshold window", redsOnly)
+
 
 img = 'armlab_opencv_examples-master/image_all_blocks.png'
 imgD = 'armlab_opencv_examples-master/depth_all_blocks.png'
@@ -45,14 +64,22 @@ cv2.imshow("Image window", rgb_image)
 
 
 # we have the masked depth and area, now colorize the mask
-imgMasked = cv2.bitwise_and(rgbraw,rgb_image,mask=thresh)
+imgMasked_rgb = cv2.bitwise_and(rgbraw,rgbraw,mask=thresh)
+imgMasked_hsv = cv2.cvtColor(imgMasked_rgb,cv2.COLOR_BGR2HSV)
 # cv2.imshow("Threshold window", thresh)
-cv2.imshow("Threshold window", imgMasked)
+# cv2.imshow("Threshold window", imgMasked_rgb)
 
-print(rgb_image.shape)
-print(depth_data.shape)
+redLowHSV = np.array([20,0,0])
+redHighHSV = np.array([100,255,255])
+RedMask = cv2.inRange(imgMasked_hsv,redLowHSV,redHighHSV)
+redsOnly = cv2.bitwise_and(imgMasked_rgb,imgMasked_rgb,mask=RedMask)
 
+cv2.imshow("Threshold window", redsOnly)
+cv2.createTrackbar("Hue High Limit", "Threshold window" , 0, 255, on_trackbar_hueHI)
+cv2.createTrackbar("Hue Low Limit", "Threshold window" , 0, 255, on_trackbar_hueLO)
 
-k = cv2.waitKey(0)
-if k == 27:
-    cv2.destroyAllWindows()
+while True:
+  k = cv2.waitKey(10)
+  if k == 27:
+    break
+cv2.destroyAllWindows()
