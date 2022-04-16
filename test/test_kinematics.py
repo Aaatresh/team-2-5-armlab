b0f6e2e1eddccea326d61cb604f6963f0f3e805e
jproney <jproney@umich.edu>
2022-04-16 15:35:44 -0400

    l

--- test/test_kinematics.py
+++ test/test_kinematics.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     screws = np.array([xi1, xi2, xi3, xi4, xi5])
     gst0 = np.array([[1.0, 0.0, 0.0, 0.0],[0.0, 1.0, 0.0, 424.15],[0.0, 0.0, 1.0, 303.91],[0.0, 0.0, 0.0, 1]])
     ### Add arm configurations to test here
-    fk_angles = [[-1.43, -0.13, -0.39, -1.31, -1.4]]
+    fk_angles = [[0.762, 1.009, 0.239, 0.756, -0.0215]]
 
     print('Test FK')
     fk_poses = []
@@ -48,72 +48,9 @@ if __name__ == '__main__':
     # print(th_d)
 
     print('Test IK 2')
-    pos = np.array([400, 400, 0, -np.pi/2, 0, 0])
+    pos = np.array([-339, 325, 22, 0, 0, 0.812])
     th_d = IK_pox(pos)
     print('th_d = ')
     print(th_d)
 
-    # config_object["waist"] = {
-    #     "P": 640,
-    #     "I": 0,
-    #     "D": 3600
-    # }
-
-    # config_object["shoulder"] = {
-    #     "P": 800,
-    #     "I": 0,
-    #     "D": 0
-    # }
-
-    # config_object["elbow"] = {
-    #     "P": 800,
-    #     "I": 0,
-    #     "D": 0
-    # }
-
-    # config_object["wrist_angle"] = {
-    #     "P": 800,
-    #     "I": 0,
-    #     "D": 0
-    # }
-
-    # config_object["wrist_rotate"] = {
-    #     "P": 640,
-    #     "I": 0,
-    #     "D": 3600
-    # }
-
-    # with open('pid.ini', 'w') as conf:
-    #     config_object.write(conf)
-    config_object = ConfigParser()
-    config_object.read("pid.ini")
-    waist = config_object["waist"]
-    shoulder = config_object["shoulder"]
-    elbow = config_object["elbow"]
-    wrist_angle = config_object["wrist_angle"]
-    wrist_rotate = config_object["wrist_rotate"]
-
-    print("Waist PID Params:")
-    print("P: ", waist["p"])
-    print("I: ", waist["i"])
-    print("D: ", waist["d"])
-
-    print("shoulder PID Params:")
-    print("P: ", shoulder["p"])
-    print("I: ", shoulder["i"])
-    print("D: ", shoulder["d"])
-
-    print("elbow PID Params:")
-    print("P: ", elbow["p"])
-    print("I: ", elbow["i"])
-    print("D: ", elbow["d"])
-
-    print("wrist_angle PID Params:")
-    print("P: ", wrist_angle["p"])
-    print("I: ", wrist_angle["i"])
-    print("D: ", wrist_angle["d"])
-
-    print("wrist_rotate PID Params:")
-    print("P: ", wrist_rotate["p"])
-    print("I: ", wrist_rotate["i"])
-    print("D: ", wrist_rotate["d"])
+    
